05cbf1a31220a0bc324d12838e42980d2f8cbab4
Gabrielle Hollaender <gabrielle.hollaender@ontariotechu.net>
2022-11-23 16:07:38 -0500

    Assets & Metas

---
+++ Latching Mechanism Simulation/Assets/Scripts/HandFlip.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tobii.XR;
+using UnityEngine;
+
+public class HandFlip : MonoBehaviour
+{
+    // The goal of this script is to make sure that the 'Neck Grab' transform appropriately switches when the other hand is used
+    
+    public gam
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
